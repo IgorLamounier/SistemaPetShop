60b08cf473ef209d32e94fdda5a50eeab8d6e6ce
IgorLamounier <igorlamounier965431@gmail.com>
2024-10-29 20:03:47 -0300

    Update MainPage.java

--- SistemaPetShop/src/pet/window/MainPage.java
+++ SistemaPetShop/src/pet/window/MainPage.java
@@ -37,7 +37,7 @@ public class MainPage extends JFrame {
 	 * Create the frame.
 	 */
 	public MainPage() {
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1080, 720);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));

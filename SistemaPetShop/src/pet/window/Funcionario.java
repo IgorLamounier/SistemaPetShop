f2833ed36feb66bf72c5f5679b063689ca3a8613
IgorLamounier <igorlamounier965431@gmail.com>
2024-10-29 21:38:34 -0300

    Update Funcionario.java

--- SistemaPetShop/src/pet/window/Funcionario.java
+++ SistemaPetShop/src/pet/window/Funcionario.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class Funcionario extends JFrame {
 
@@ -13,9 +15,21 @@ public class Funcionario extends JFrame {
     private JTextField txtEndereco;
     private JTextField txtTelefone;
     private JTextField txtEmail;
-    private JTextField txtCargo;
     private JButton btnCadastrar;
+    private JComboBox<String> cmbCargo;
+    private JTextField txtData;
 
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+            	Funcionario frame = new Funcionario();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+    
     public Funcionario() {
         setSize(1080, 720);
         setResizable(false);
@@ -63,12 +77,9 @@ public class Funcionario extends JFrame {
         txtEmail = new JTextField();
         txtEmail.setBounds(210, 194, 300, 30);
         txtEmail.setFont(font);
-        txtCargo = new JTextField();
-        txtCargo.setBounds(210, 235, 300, 30);
-        txtCargo.setFont(font);
 
         btnCadastrar = new JButton("Cadastrar");
-        btnCadastrar.setBounds(210, 276, 150, 30);
+        btnCadastrar.setBounds(210, 317, 150, 30);
         btnCadastrar.setFont(font);
         btnCadastrar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -88,11 +99,10 @@ public class Funcionario extends JFrame {
         getContentPane().add(lblEmail);
         getContentPane().add(txtEmail);
         getContentPane().add(lblCargo);
-        getContentPane().add(txtCargo);
         getContentPane().add(btnCadastrar);
         
         JButton btnLimpar = new JButton("Limpar");
-        btnLimpar.setBounds(372, 276, 150, 30);
+        btnLimpar.setBounds(372, 317, 150, 30);
         btnLimpar.setFont(new Font("Arial", Font.PLAIN, 16));
         getContentPane().add(btnLimpar);
         
@@ -105,6 +115,26 @@ public class Funcionario extends JFrame {
         });
         btnVoltar.setFont(new Font("Arial", Font.PLAIN, 16));
         getContentPane().add(btnVoltar);
+        
+        cmbCargo = new JComboBox<>();
+        cmbCargo.addItem("Veterinário");
+        cmbCargo.addItem("Atendente");
+        cmbCargo.addItem("Gerente");
+        cmbCargo.addItem("Recepcionista");
+        cmbCargo.addItem("Assistente");
+        cmbCargo.setBounds(210, 235, 300, 30);
+        getContentPane().add(cmbCargo);
+        
+        JLabel lblData = new JLabel("Data de Admissão:");
+        lblData.setHorizontalAlignment(SwingConstants.RIGHT);
+        lblData.setFont(new Font("Arial", Font.PLAIN, 16));
+        lblData.setBounds(50, 276, 150, 30);
+        getContentPane().add(lblData);
+        
+        txtData = new JTextField();
+        txtData.setFont(new Font("Arial", Font.PLAIN, 16));
+        txtData.setBounds(210, 276, 300, 30);
+        getContentPane().add(txtData);
         
         JLabel lblNewLabel = new JLabel("");
         lblNewLabel.setIcon(new ImageIcon(Funcionario.class.getResource("/img/Fundo2.png")));
@@ -120,28 +150,43 @@ public class Funcionario extends JFrame {
         String endereco = txtEndereco.getText().trim();
         String telefone = txtTelefone.getText().trim();
         String email = txtEmail.getText().trim();
-        String cargo = txtCargo.getText().trim();
+        String cargo = cmbCargo.getSelectedItem().toString();
+        String data = txtData.getText();
 
-        if (nome.isEmpty() || cpf.isEmpty() || endereco.isEmpty() || telefone.isEmpty() || email.isEmpty() || cargo.isEmpty()) {
+        if (nome.isEmpty() || cpf.isEmpty() || endereco.isEmpty() || telefone.isEmpty() || email.isEmpty() || cargo.isEmpty() || data.isEmpty()) {
             JOptionPane.showMessageDialog(this, "Todos os campos são obrigatórios!", "Erro", JOptionPane.ERROR_MESSAGE);
-            return;
         }
-
-
-        JOptionPane.showMessageDialog(this, "Funcionário cadastrado:\nNome: " + nome +
-                "\nCPF: " + cpf + "\nTelefone: " + telefone + "\nE-mail: " + email +
-                "\nCargo: " + cargo);
+        
+        if (addFuncionarioDatabase(nome, cpf, endereco, telefone, email, cargo, data)) {
+			JOptionPane.showMessageDialog(this, "Funcionário cadastrado com sucesso!");
+		}
 
        
-        txtNome.setText("");
-        txtCPF.setText("");
-        txtEndereco.setText("");
-        txtTelefone.setText("");
-        txtEmail.setText("");
-        txtCargo.setText("");
     }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new Funcionario());
+    
+    private boolean addFuncionarioDatabase(String nome, String cpf, String endereco, String telefone, String email, String cargo, String data) {
+		boolean sucess = false;
+		
+		try {
+			Connection conexao = ConnectionFactory.createConnection();
+			String sql = "insert into funcionarios (nome, cpf, endereco, telefone, email, cargo, data_admissao) values (?, ?, ?, ?, ?, ?, ?);";
+			PreparedStatement cmd = conexao.prepareStatement(sql);
+			
+			cmd.setString(1, nome);
+			cmd.setString(2, cpf);
+			cmd.setString(3, endereco);
+			cmd.setString(4, telefone);
+			cmd.setString(5, email);
+			cmd.setString(6, cargo);
+			cmd.setString(7, data);
+			
+			int rowAffected = cmd.executeUpdate();
+			sucess = rowAffected > 0;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return sucess;
+    
     }
 }
